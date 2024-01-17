a0ca700532de8ca795e42c607d3083bca5b3aba9
Diego Garcia <garciadss100@gmail.com>
2024-01-17 03:10:30 -0500

    add some logic to the btn when no number is checked

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -15,7 +15,11 @@ console.log(document.querySelector('.guess').value);
 */
 
 document.querySelector('.check').addEventListener('click', function () {
-  const guess = document.querySelector('.guess').value;
+  const guess = Number(document.querySelector('.guess').value);
 
-  console.log(guess);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
 });
