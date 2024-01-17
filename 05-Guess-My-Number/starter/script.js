2710d3d6bb06671f469cf6f2da2cfd892b080273
Diego Garcia <garciadss100@gmail.com>
2024-01-17 01:40:25 -0500

    change the number and the highscore

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -5,3 +5,6 @@ console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉 Correct Number!';
 
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
