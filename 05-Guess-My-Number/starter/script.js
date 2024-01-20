92b01e69a0462fb21c8ba36d388fdba24aea9036
Diego Garcia <garciadss100@gmail.com>
2024-01-20 01:32:31 -0500

    comment out the use strict for the mean time

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';
 
 /*
 console.log(document.querySelector('.message').textContent);
@@ -62,8 +62,7 @@ document.querySelector('.check').addEventListener('click', function () {
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
-
+  secretNummber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('.message').textContent = 'Start guessing...';
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
