68a69310a8e57a2338db9e57b5d9720cf0ba737a
Diego Garcia <garciadss100@gmail.com>
2024-01-20 00:37:57 -0500

    move the secret number, that way the ? is visible agian

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -15,7 +15,6 @@ console.log(document.querySelector('.guess').value);
 */
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
 
 let score = 20;
 
@@ -30,6 +29,8 @@ document.querySelector('.check').addEventListener('click', function () {
     // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
 
