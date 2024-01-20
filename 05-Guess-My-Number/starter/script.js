ea3ddab63e67dea2498fcdab569b9cbb77672cbf
Diego Garcia <garciadss100@gmail.com>
2024-01-20 00:00:42 -0500

    create a green background when you win and increase font width

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -31,6 +31,10 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
 
+    documant.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
     // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
